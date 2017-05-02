3c9c0f3039231ce8f6cb78e425f58c70b1a9a0e7
Allen Wipf <allenwipf@msn.com>
2017-05-02 17:07:24 -0500

    finished Geo_ca model. have to refactor controllers and views

--- app/models/geo_ca.rb
+++ app/models/geo_ca.rb
@@ -1,18 +1,24 @@
 class Geo_ca
 	include ActiveModel::Model
 
+	attr_accessor :city
+	attr_accessor :state
+	attr_accessor :zip
+	attr_accessor :confidence
+	attr_accessor :longt
+	attr_accessor :latt
+	
+
+# Creates a Geo_ca Object based on the zip code entered.
 	def initialize(zip = 68117)
      
     zipInfo = Geocoder.search(zip)
 
-		@city = 1
-		@state =2
-		@weather = 2
-
+		@city = zipInfo[0].data["standard"]["city"]
+		@state = zipInfo[0].data["standard"]["prov"]
+		@zip = zipInfo[0].data["standard"]["postal"]
+		@confidence = zipInfo[0].data["standard"]["confidence"]
+		@longt = zipInfo[0].data["longt"]
+		@latt = zipInfo[0].data["latt"]
 	end
-
-
-
-
-
 end
